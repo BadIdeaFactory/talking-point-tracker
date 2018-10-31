22bc297854571a45e25e1d81849f5a37b870a813
Dan Schultz <slifty@gmail.com>
2018-10-30 23:19:08 -0400

    Add sentence extraction and truecase
    This is two commits, but I'm going to combine it here since it is late.
    Don't tell future Dan.
    
    Boy, NLP sure is fancy!  Who knew that breaking things into sentences
    could be so complicated!  Since caption streams have unpredictable
    cases, we needed to try to standardize that input before doing things
    like identifying sentences and named entity recognition.  There are two
    passes at the capitalization problem.
    
    The first is stupid, it just lower cases everything, and then upper
    cases prefixes like Mr. and Mrs. to increase the odds that those won't
    be incorrectly mistaken as the end of a sentence (note: we should do
    a better job of guaranteeing that won't happen, as well as creating
    a more comprehensive list of prefixes).
    
    Then, we break into sentences.
    
    Then, we take each sentence and run it against a "Truecase" algorithm,
    specifically this one: https://github.com/nreimers/truecaser
    
    Right now I've been using the default model for truecaser and it seems
    pretty legit, but frankly the entire module has room for improvement,
    and the model definitely does.
    
    At any rate, this pipeline has dramatically increased the "at a glance"
    accurace of the named entity recognition, and that's pretty exciting to
    me!
    
    Here is some example output:
    
    [1] He voted 11 times to repeal the ACA with no replacement, which illuminates people with pre-existing conditions from being discriminate against Avenue should look at the AARP and American Cancer Society, who agree with this.--against.[11 (CARDINAL) ACA (ORG) Avenue (PRODUCT) AARP (ORG) American Cancer Society (ORG)]
    [1] You should look at the AARP and the American Cancer Society.[AARP (ORG) the American Cancer Society (ORG)]
    [1] The bill he supported would not have helped people with pre-existing conditions.[]
    [1] Please do fact check that.[]
    [1] The AARP, the American Cancer Society, they all said that.[AARP (ORG) the American Cancer Society (ORG)]
    [1] I am agreeing with them on that.[]
    
    Badass.
    
    Issue #8 Break captions into sentences

--- src/server/workers/opened-captions/index.js
+++ src/server/workers/opened-captions/index.js
@@ -2,7 +2,8 @@ import OpenedCaptions from 'opened-captions'
 import waitOn from 'wait-on'
 import rp from 'request-promise'
 
-const buffer = []
+let buffer = []
+let sentencesPromise = null
 
 function start() {
   console.log("STARTING")
@@ -16,12 +17,27 @@ function start() {
     });
 
     oc.processWord = function(content, stream) {
-      newWord(content)
+      let word = toProperCase(content)
+      newWord(word)
     }
   }).catch(function (err) {
     console.log(err)
-    console.log("zomg")
   })
+}
+
+function toProperCase(word) {
+  let cleanedWord = word
+
+  if(!cleanedWord.match(/(.?\..?){2,}/) // Don't lowercase acronyms e.g. "U.S."
+  && cleanedWord != "I") {          // Don't lowercase "I"
+    cleanedWord = cleanedWord.toLowerCase()
+  }
+
+  if(cleanedWord.match(/(mr\.)|(dr\.)|(mrs\.)|(sen\.)|(ms\.)|(rep\.)/i)) {
+    cleanedWord = cleanedWord.charAt(0).toUpperCase() + cleanedWord.slice(1)
+  }
+
+  return cleanedWord
 }
 
 function waitForNlp() {
@@ -32,18 +48,92 @@ function waitForNlp() {
 }
 
 function newWord(word) {
-  // Do something with the word spoken on TV here.
-  // console.log(word)
-  if(buffer.length < 10) {
-    buffer.push(word)
+  buffer.push(word)
+  if(buffer.length > 20
+  && sentencesPromise == null) {
+    sentencesPromise = getSentences(buffer).then((sentences) => {
+      const cleanedSentences = sentences.map(cleanContent)
+      if(cleanedSentences.length > 1) {
+        processSentence(cleanedSentences.shift())
+      }
 
-  } else {
-    const line = buffer.join(" ")
-    getNlpDep(line).then((data) => {
-      console.log(data)
+      // Add the sentences back onto the queue
+      buffer = cleanedSentences.concat(buffer)
+      sentencesPromise = null
     })
     buffer.length = 0
   }
+}
+
+function cleanContent(content) {
+  let cleanedContent = content
+    .replace(/\s+([\u2000-\u206F\u2E00-\u2E7F\\'!"#%&()*+,\-.\/:;<=>?@\[\]^_`{|}~])/g, "$1") // Punctuation may have spaces inserted before it
+    .replace(/([-])\s+/g, "$1") // hyphens should not have spaces around it
+    .replace(/(.*) n't/g, "$1n't") // n't gets split into its own token
+    .replace(">>", "")
+    .trim()
+  cleanedContent = capitalizeNames(cleanedContent)
+  return cleanedContent
+}
+
+function processSentence(sentence) {
+  getTruecase(sentence).then((sentence) =>
+    getEnt(sentence).then((ents) => {
+      ents = ents.map((ent) => {
+        return sentence.substring(ent.start, ent.end) + " (" + ent.label + ")"
+      })
+
+      console.log(cleanContent(sentence) + "[" + ents.join(" ") + "]")
+
+    })
+  )
+}
+
+function capitalizeNames(text) {
+  text.replace(/(mr\.)|(dr\.)|(mrs\.)|(sen\.)|(ms\.)|(rep\.)\s./ig, function(l) {
+    return l.substring(0, l.length) + l.substr(l.length).toUpperCase()
+  })
+  return text
+}
+
+function getTruecase(sentence) {
+  var options = {
+      method: 'POST',
+      uri: 'http://localhost:8081/truecase',
+      body: {
+          "text": sentence,
+          "model":"en_core_web_lg"
+      },
+      json: true // Automatically stringifies the body to JSON
+  };
+  return rp.post(options)
+}
+
+function getSentences(buffer) {
+  var options = {
+      method: 'POST',
+      uri: 'http://localhost:8081/sentences',
+      body: {
+          "text": cleanContent(buffer.join(" ")),
+          "model":"en_core_web_lg"
+      },
+      json: true // Automatically stringifies the body to JSON
+  };
+  return rp.post(options)
+}
+
+function getEnt(sentence) {
+  var options = {
+      method: 'POST',
+      uri: 'http://localhost:8081/ent',
+      body: {
+          "text": sentence,
+          "model":"en_core_web_lg"
+      },
+      json: true // Automatically stringifies the body to JSON
+  };
+  return rp.post(options)
+
 }
 
 function getNlpDep(line) {
